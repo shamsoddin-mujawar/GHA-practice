b40e659bfe1bc0bf716b393c4005ffa5e885f94e
shamsoddin-mujawar <85243080+shamsoddin-mujawar@users.noreply.github.com>
2023-10-10 16:25:29 +0530

    Update first.py

--- .github/actions/testing/first.py
+++ .github/actions/testing/first.py
@@ -1,9 +1,11 @@
 import os
+import subprocess
 
 from common.second import fun
 
 num1 = os.getenv('INPUT_A')
 num2 = os.getenv('INPUT_B')
-
+cmd = "pwd"
+subprocess.call(cmd, shell=True)
 s = fun(num1, num2)
 print(s)
